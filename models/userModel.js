9426b12458ce2516b227248a762f47481d5ed3b4
Raghad Islam <raghad.noser.2003@gmail.com>
2025-04-29 05:59:21 +0300

    feat(auth): add password management with bcrypt hashing

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,7 +29,22 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Pleas confirm the password'],
+    validate: {
+      // This only works on SAVE and CEATE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Confirmed password is not the same as Password!',
+    },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
