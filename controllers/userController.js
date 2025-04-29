e41ed4d0d6bb655a04ba933fd53b5b06912c1347
Raghad Islam <raghad.noser.2003@gmail.com>
2025-04-29 07:30:39 +0300

    feat(auth): add JWT auth, signup, and login endpoints

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,18 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    // 500 means internal server error
-    status: 'error',
-    message: 'this route is not yet defined',
+const User = require('../models/userModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getAllUsers = catchAsync(async (req, res) => {
+  const allUser = await User.find();
+
+  // SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    results: allUser.length,
+    data: {
+      allUser,
+    },
   });
-};
+});
 
 exports.getUser = (req, res) => {
   res.status(500).json({
