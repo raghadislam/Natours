4893e78adda71dd357db3673d7bcef3358095851
Raghad Islam <raghad.noser.2003@gmail.com>
2025-05-04 05:42:48 +0300

    feat(user): implement profile data updates

--- controllers/authController.js
+++ controllers/authController.js
@@ -178,14 +178,13 @@ exports.updatePassword = catchAsync(async (req, res, next) => {
 
   // 2) Check if POSTed current password is correct
   if (!(await user.correctPassword(req.body.passwordCurrent, user.password))) {
-    return next(new AppError('Your current password is wrong.', 401));
+    return next(new AppError('Your provided password is wrong.', 401));
   }
 
   // 3) If so, update password
   user.password = req.body.password;
   user.passwordConfirm = req.body.passwordConfirm;
   await user.save();
-  // User.findByIdAndUpdate will NOT work as intended!
 
   // 4) Log user in, send JWT
   createSendToken(user, 200, res);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const allUser = await User.find();
@@ -10,6 +19,31 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     results: allUser.length,
     data: {
       allUser,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create Error if user posts password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates, Plase use /updateMyPassword',
+        400,
+      ),
+    );
+  }
+  // 2) Filter out the unubdateable fields and update the user data
+  const filteredObj = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredObj, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    date: {
+      user: updatedUser,
     },
   });
 });
