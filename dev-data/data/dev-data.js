d7b4aea1bb6244a90279f959bd9b72c46d526de2
Raghad Islam <raghad.noser.2003@gmail.com>
2025-05-07 11:40:58 +0300

    feat(auth): added missing authentication and authorization and imported some data

--- dev-data/data/dev-data.js
+++ dev-data/data/dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
 
 dotenv.config({ path: './config.env' });
 
@@ -20,10 +22,14 @@ mongoose
   .then(() => console.log('DB conneted!'));
 
 const data = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/reviews.json`));
 
 const importData = async () => {
   try {
     await Tour.create(data);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Data successfully loaded!');
     process.exit();
   } catch (err) {
@@ -34,6 +40,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data successfully deleted!');
     process.exit();
   } catch (err) {
