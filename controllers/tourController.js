87fe0a711c986bd2e85a46de3c8b59ba7c9fef08
Raghad Islam <raghad.noser.2003@gmail.com>
2025-05-08 10:01:36 +0300

    feat(geo): implement tour distance aggregation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -95,13 +95,12 @@ exports.getTourWithin = catchAsync(async (req, res, next) => {
   const { distance, latlng, unit } = req.params;
   const [lat, lng] = latlng.split(',');
   const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
-  if (!lat || !lng)
+  if (!lat || !lng) {
     return new AppError(
       'Please provide latitude and longitude in the format lat,lng',
       400,
     );
-
-  // console.log(distance, lat, lng, unit);
+  }
 
   const tours = await Tour.find({
     startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
@@ -112,6 +111,46 @@ exports.getTourWithin = catchAsync(async (req, res, next) => {
     results: tours.length,
     data: {
       data: tours,
+    },
+  });
+});
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multiplier = unit === 'mi' ? 0.0006213712 : 0.001;
+
+  if (!lat || !lng) {
+    return new AppError(
+      'Please provide latitude and longitude in the format lat,lng',
+      400,
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: distances,
     },
   });
 });
