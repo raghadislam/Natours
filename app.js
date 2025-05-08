12eea969457e7f10500eb8426f258d808f793b63
Raghad Islam <raghad.noser.2003@gmail.com>
2025-05-08 10:43:17 +0300

    feat(views): integrate Pug template engine

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
@@ -14,7 +15,13 @@ const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
 // 1) GLOBAL MIDDLEWARES
+// Serve the static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // Set security HTTP headers
 app.use(helmet());
 
@@ -56,9 +63,6 @@ app.use(
   }),
 );
 
-// Serve the static files
-app.use(express.static(`${__dirname}/public`));
-
 // a Middleware to set the request time in the request object
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -66,6 +70,13 @@ app.use((req, res, next) => {
 });
 
 // Routes
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Jonas',
+  });
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
