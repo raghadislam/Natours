e7cc913b1f552e4e059b5f773113024bbbfa7a44
Raghad Islam <raghad.noser.2003@gmail.com>
2025-05-09 06:30:37 +0300

    feat(views): Built a dynamic tour page

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,5 +1,6 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
+// const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
@@ -10,8 +11,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour', {
-    title: 'The Forest Hiker',
+exports.getTour = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: ' review rating user',
   });
-};
+
+  //   if (!doc) {
+  //     return next(new AppError('No document found with that ID', 404));
+  //   }
+
+  res.status(200).render('tour', {
+    title: tour.slug,
+    tour,
+  });
+});
