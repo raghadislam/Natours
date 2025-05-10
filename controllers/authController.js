091a7ef4bb143ebe877ab619c881936185f5b295
Raghad Islam <raghad.noser.2003@gmail.com>
2025-05-10 07:01:46 +0300

    Reapply "implemented full gogging in front end logic"
    This reverts commit 016847c82a21ddc127f1af0f2a32dc323646934f.

--- controllers/authController.js
+++ controllers/authController.js
@@ -63,6 +63,8 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
 
   if (!token)
@@ -95,6 +97,32 @@ exports.protect = catchAsync(async (req, res, next) => {
 
   // Grant access to the prote ted route
   req.user = currentUser;
+  next();
+});
+
+// Onlu for renderd pages, no errors
+exports.isLoggedin = catchAsync(async (req, res, next) => {
+  if (req.cookies.jwt) {
+    // 1) Verify the token
+    const decoded = await promisify(jwt.verify)(
+      req.cookies.jwt,
+      process.env.JWT_SECRET,
+    );
+
+    // 2) Check if the user still exists
+    const currentUser = await User.findById(decoded.id);
+    if (!currentUser) {
+      return next();
+    }
+
+    // 3) Check if the user changed his password after the token was issued
+    if (currentUser.changedPasswordAfter(decoded.iat)) {
+      return next();
+    }
+
+    // There is a loggedin user
+    res.locals.user = currentUser;
+  }
   next();
 });
 
