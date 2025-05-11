16dd3fb283a359277df2cb2edd9b852f09fc602a
Raghad Islam <raghad.noser.2003@gmail.com>
2025-05-11 21:52:40 +0300

    feat: Built a complex real email handler

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -13086,7 +13086,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40995" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32973" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
