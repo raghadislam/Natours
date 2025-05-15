83bdfea715df4048f2e26972f2f761b90c757f54
Raghad Islam <raghad.noser.2003@gmail.com>
2025-05-16 02:08:42 +0300

    enhanced stripe functionality 2

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -42,10 +42,12 @@ const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.find({ email: session.customer_email })).id;
   const price = session.amount_total / 100;
+  console.log('These are the values', tour, user, price);
+
   await Booking.create({ tour, user, price });
 };
 
-exports.webhookCheckout = (req, res, next) => {
+exports.webhookCheckout = async (req, res, next) => {
   const signature = req.headers['stripe-signature'];
 
   let event;
@@ -59,8 +61,10 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`Webhook error: ${err.message}`);
   }
 
+  console.log('event type:', event.type);
+
   if (event.type === 'checkout.session.completed')
-    createBookingCheckout(event.data.object);
+    await createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
 };
